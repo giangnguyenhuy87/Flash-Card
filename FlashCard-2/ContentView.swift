5da87afee7f8d7db7ef51824e4160ea13d0e8c8b
Huy Giang <huygiang099@gmail.com>
2026-01-27 18:09:31 +0700

    Initial Commit

---
+++ FlashCard-2/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  FlashCard-2
+//
+//  Created by Giang Huy on 27/1/26.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ FlashCard-2/FlashCard_2App.swift
@@ -0,0 +1,17 @@
+//
+//  FlashCard_2App.swift
+//  FlashCard-2
+//
+//  Created by Giang Huy on 27/1/26.
+//
+
+import SwiftUI
+
+@main
+struct FlashCard_2App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
